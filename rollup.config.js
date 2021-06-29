921780735ede499fd7ccd790be6e067e1d4047a6
Lukasz Celitan <lukasz.celitan@gmail.com>
2021-06-29 23:20:28 +0200

    upadate packages

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,8 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
-import svelteSVG from 'rollup-plugin-svelte-svg'
+import { svelteSVG } from "rollup-plugin-svelte-svg";
+import css from 'rollup-plugin-css-only'
 
 // eslint-disable-next-line no-undef
 const production = !process.env.ROLLUP_WATCH
@@ -19,24 +20,21 @@ export default {
   plugins: [
     svelteSVG(),
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file - better for performance
-      css: (css) => {
-        css.write('public/build/bundle.css')
-      },
-    }),
-
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
+			}
+		}),
+    css({ output: 'public/build/bundle.css' }),
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
     // some cases you'll need additional configuration -
     // consult the documentation for details:
     // https://github.com/rollup/plugins/tree/master/packages/commonjs
-    resolve({
-      browser: true,
-      dedupe: ['svelte'],
-    }),
+		resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
     commonjs(),
 
     // In dev mode, call `npm run start` once
